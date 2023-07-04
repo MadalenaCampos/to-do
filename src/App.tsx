b6f4e0a53124eb6fa101da5fe2e2a50b5b035abb
Madalena Campos <madacampos02@gmail.com>
2023-07-04 09:43:29 -0300

    criando header do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import rocketLogo from "./assets/rocket.svg";
+import todoLogo from "./assets/todo.svg";
 
 function App() {
   return (
     <div>
-      <input type="checkbox" id="myCheckbox" />
-      <label htmlFor="myCheckbox"></label>
+      <div className="header">
+        <div className="logo">
+          <img src={rocketLogo} alt="logo" />
+          <img src={todoLogo} alt="logo" />
+        </div>
+      </div>
     </div>
   );
 }

5c333565ec224faf9e64f3270f30aa48ab9a916c
Madalena Campos <madacampos02@gmail.com>
2023-08-02 13:53:55 -0300

    ajustando concluidos e readme

--- src/components/Count/Count.tsx
+++ src/components/Count/Count.tsx
@@ -11,9 +11,19 @@ export function Count({ tarefasCriadas, concluidas }: CountProps) {
       <p>
         Tarefas criadas <span>{tarefasCriadas | 0}</span>
       </p>
-      <p>
-        Concluídas <span>{concluidas | 0}</span>
-      </p>
+
+      {(tarefasCriadas > 0 && (
+        <p>
+          Concluídas{" "}
+          <span>
+            {concluidas | 0} de {tarefasCriadas}
+          </span>
+        </p>
+      )) || (
+        <p>
+          Concluídas <span>{concluidas | 0}</span>
+        </p>
+      )}
     </div>
   );
 }
